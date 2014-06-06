0470900a28d22a0d29c1f398c516c0c1776c9da6
David Aragon <david@quickleft.com>
2014-06-05 21:00:38 -0600

    space_utilization.sh

--- space_utilization.sh
+++ space_utilization.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-percentage_utilized=$(df -h | sed -n 2p | cut -d’ ‘ -f9 | cut -d% -f1)
-email_body_string=”Utilization is at $percentage_utilized%”
+percentage_utilized=$(df -h | sed -n 2p | cut -d' ' -f9 | cut -d% -f1)
+email_body_string="Utilization is at $percentage_utilized%"
 touch email_body_file.txt
 echo $email_body_string > email_body_file.txt
 if [ $percentage_utilized -gt 79 ] 
 then 
+	echo "Mailing admin"
 	mail -s “Utilization is high” root < email_body_file.txt
+else
+	echo "Utilization acceptable"
 fi
